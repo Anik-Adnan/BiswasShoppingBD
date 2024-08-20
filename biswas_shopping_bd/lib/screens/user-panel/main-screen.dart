c5104a216f38438941f316d518e37f2abe3d07b4
Anik Adnan <anikadnanict17@gmail.com>
2024-08-21 01:16:39 +0600

    drawerWidget added

--- biswas_shopping_bd/lib/screens/user-panel/main-screen.dart
+++ biswas_shopping_bd/lib/screens/user-panel/main-screen.dart
@@ -1,10 +1,8 @@
 
-import 'package:biswas_shopping_bd/screens/auth-ui/welcome-screen.dart';
-import 'package:biswas_shopping_bd/utils/app-constant.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import '/utils/app-constant.dart';
+import '/widgets/drawer-widget.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+
 class MainScreen extends StatelessWidget{
   const MainScreen({super.key});
 
@@ -15,21 +13,8 @@ class MainScreen extends StatelessWidget{
         backgroundColor: AppConstant.appMainColor,
         title: Text(AppConstant.appName,style: const TextStyle(fontWeight: FontWeight.bold),),
         centerTitle: true,
-        actions: [
-          GestureDetector(
-            onTap: () async{
-              GoogleSignIn googleSignIn = GoogleSignIn();
-              FirebaseAuth auth = FirebaseAuth.instance;
-              await auth.signOut();
-              await googleSignIn.signOut();
-              Get.offAll(()=> WelcomeScreen());
-            },
-            child: const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Icon(Icons.logout)),
-          ),
-        ],
       ),
+      drawer: const DrawerWidget(),
 
     );
   }
